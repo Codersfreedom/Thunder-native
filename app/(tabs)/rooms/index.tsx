22376f28d314d7ef1a29398960ff3fc4946c3b7d
rakesh <rakeshmanna762@gmail,com>
2025-01-25 21:24:08 +0530

    frontend: rooms page designed

--- app/(tabs)/rooms/index.tsx
+++ app/(tabs)/rooms/index.tsx
@@ -1,11 +1,37 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { VStack } from "@/components/ui/vstack";
+import { ScrollView } from "react-native-gesture-handler";
+import JoinedRoom from "../../../components/rooms/JoinedRoom";
+import PublicRoom from "../../../components/rooms/PublicRoom";
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <VStack space="xl" className="mt-16 dark:bg-dark-background h-screen">
+      {/* Public rooms section */}
+      <ScrollView
+        horizontal={true}
+        showsVerticalScrollIndicator={false}
+        className="min-h-36 px-3 "
+      >
+        <PublicRoom />
+        <PublicRoom />
+        <PublicRoom />
+        <PublicRoom />
+        <PublicRoom />
+        <PublicRoom />
+      </ScrollView>
+      {/* Joined rooms section */}
+      <ScrollView className="p-3 ">
+        <JoinedRoom />
+        <JoinedRoom />
+        <JoinedRoom />
+        <JoinedRoom />
+        <JoinedRoom />
+        <JoinedRoom />
+        <JoinedRoom />
+        <JoinedRoom />
+      </ScrollView>
+    </VStack>
   );
 };
 
